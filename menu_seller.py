745161be741f12c4cd5d4c66ed3088a22943ad5f
KhanzaHumaira29 <khanzahumaira824@gmail.com>
2025-11-17 10:47:38 +0800

    menuSeller

--- menu_seller.py
+++ menu_seller.py
@@ -1,1 +1,28 @@
-print("TES")
+import os
+import inquirer
+
+current_seller = {
+    "username": "seller1",
+    "nama": "Khanza",
+    "email": "seller1@gmail.com",
+    "no_hp": "08123456789"
+}
+
+# daftar produk popcorn
+produk_list = [
+    {"id": 1, "varian": "Caramel", "kemasan": "Small", "harga": 5000, "status": "Tersedia"},
+    {"id" : 2, "varian": "Matcha", "kemasan" : "Medium", "harga" : 10000, "status": "Tersedia"},
+    {"id" : 3, "varian": "Choco", "kemasan" : "Large", "harga" : 15000, "status": "Habis"}
+]
+
+# daftar pesanan (dari user)
+pesanan_list = [
+    {
+    "id_pesanan": 1,
+    "nama_user": "Ghina",
+    "produk": "Caramel - Medium",
+    "jumlah": 2,
+    "total_harga": 20000,
+    "status_pesanan": ""
+    }
+]
