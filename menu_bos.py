f9409ae4fc41caec1b01d9e5aa2ba9570c23122d
shasha1050 <mainshaa.44@gmail.com>
2025-11-17 10:40:17 +0800

    nambah banyak bos

--- menu_bos.py
+++ menu_bos.py
@@ -1,24 +1,41 @@
 import inquirer
 from help import jud_utama, jud_sub, pesan_berhasil, pesan_peringatan
+from fungsi_umum import daftar_akun, daftar_produk
 
 def menu_boss():
-    jud_utama()
-    jud_sub("Selamat Datang Bos!")
     while True:
+        jud_utama()
+        jud_sub("Selamat Datang Bos!")
         questions = [
             inquirer.List(
                 "menu",
                 message="Pilih menu:",
                 choices=[
-                    "1. Akun",
+                    "1. Daftar Akun",
                     "2. Daftar Produk",
-                    "3. Logout"])]
+                    "3. Hapus Rating",
+                    "4. Laporan Penjualan",
+                    "5. Logout"])]
         answer = inquirer.prompt(questions)["menu"]
 
-        if answer == "1. Akun":
-            print("Akun")
+        if answer == "1. Daftar Akun":
+            jud_utama()
+            jud_sub("Daftar Akun")
+            daftar_akun()
+            print("")
+            print("═"*60)
+            input("→ 「 Enter untuk kembali 」")
         elif answer == "2. Daftar Produk":
-            print("Daftar Produk")
-        elif answer == "3. Logout":
+            jud_utama()
+            jud_sub("Daftar Produk")
+            daftar_produk()
+            print("")
+            print("═"*60)
+            input("→ 「 Enter untuk kembali 」")
+        elif answer == "3. Hapus Rating":
+            print("d")
+        elif answer == "4. Laporan Penjualan":
+            print("Laporan Penjualan")
+        elif answer == "5. Logout":
             pesan_berhasil("Logout Berhasil!")
             break
