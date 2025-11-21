76422b187b287fe49d4cc13234608c9edd2ad1f9
Shasha-Amaranggana <mainshaa.44@gmail.com>
2025-11-21 18:47:00 +0800

    ubah banyak lagi

--- menu_konsumen.py
+++ menu_konsumen.py
@@ -1,62 +1,55 @@
 import inquirer
 from help import jud_utama, jud_sub, pesan_berhasil
 from fungsi_konsumen import lihat_akun, lihat_produk
+
+
 def menu_konsumen(username):
     while True:
         jud_utama()
         jud_sub(f"Selamat Datang, {username}!")
-
         questions = [
             inquirer.List(
                 "menu",
                 message="Pilih menu:",
                 choices=[
-                    "1. Lihat Akun",
-                    "2. Lihat Produk",
-                    "3. Keranjang Belanja",
-                    "4. Pesanan Anda",
-                    "5. Riwayat",
-                    "6. Rating",
-                    "7. Saldo",
-                    "8. Logout"])]
-
+                    "1 | LIHAT AKUN",
+                    "2 | LIHAT PRODUK",
+                    "3 | KERANJANG BELANJA",
+                    "4 | PESANAN ANDA",
+                    "5 | RIWAYAT",
+                    "6 | RATING",
+                    "7 | SALDO",
+                    "8 | LOGOUT"])]
         answer = inquirer.prompt(questions)["menu"]
 
-        if answer == "1. Lihat Akun":
+        if answer == "1 | LIHAT AKUN":
             jud_utama()
             jud_sub("Akun Saya")
             lihat_akun(username)
-
-        elif answer == "2. Lihat Produk":
+        elif answer == "2 | LIHAT PRODUK":
             jud_utama()
             jud_sub("Daftar Produk")
             lihat_produk()
-
-        elif answer == "3. Keranjang Belanja":
+        elif answer == "3 | KERANJANG BELANJA":
             jud_utama()
             jud_sub("Keranjang Belanja")
             keranjang_belanja(username)
-
-        elif answer == "4. Pesanan Anda":
+        elif answer == "4 | PESANAN ANDA":
             jud_utama()
             jud_sub("Pesanan Anda")
             pesanan_anda(username)
-
-        elif answer == "5. Riwayat":
+        elif answer == "5 | RIWAYAT":
             jud_utama()
             jud_sub("Riwayat Belanja")
             riwayat(username)
-
-        elif answer == "6. Rating":
+        elif answer == "6 | RATING":
             jud_utama()
             jud_sub("Rating Produk")
             rating(username)
-
-        elif answer == "7. Saldo":
+        elif answer == "7 | SALDO":
             jud_utama()
             jud_sub("Saldo")
             saldo(username)
-
-        elif answer == "8. Logout":
+        elif answer == "8 | LOGOUT":
             pesan_berhasil("Logout Berhasil!")
             break
