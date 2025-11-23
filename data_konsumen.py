adae916888d01b7976816b42b0d909fa66df648f
owensurianto <owensuriantovs@gmail.com>
2025-11-23 22:02:09 +0800

    edit keranjang

--- data_konsumen.py
+++ data_konsumen.py
@@ -1,4 +1,12 @@
-keranjang = {}
+keranjang_user = {
+    "owen": {
+        "1": { "nm": "popcorn1", "sz": "L", "pz": 25000, "jumlah": 2 },
+        "3": { "nm": "popcorn3", "sz": "S", "pz": 15000, "jumlah": 1 }}}
+
+
+def init_keranjang_user(username):
+    if username not in keranjang_user:
+        keranjang_user[username] = {}
 
 pesanan = {}
 
